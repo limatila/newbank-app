86118008663ee1f2fa54a5cdd1754e8d9e1b78f4
Átila Lima <atilalimade@gmail.com>
2025-07-10 17:26:46 -0300

    api: removing git webhook from docs view

--- src/backend/routers/webhooks.py
+++ src/backend/routers/webhooks.py
@@ -6,6 +6,7 @@ from backend.services.webhooks import update_local_code
 
 webhooks_router = APIRouter(tags=["Webhooks"], prefix="/hooks")
 
-@webhooks_router.post("/git-push", summary="A webhook accessed by github on a case of a Git Push. It'll update the code based on the main git branch")
+@webhooks_router.post("/git-push", include_in_schema=False,
+                       summary="A webhook accessed by github on a case of a Git Push. It'll update the code based on the main git branch")
 def git_push_webhook(request: Request):
     return update_local_code()
