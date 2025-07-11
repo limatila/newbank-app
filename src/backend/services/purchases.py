41c2319af6ff92f028c3ab3d64b3a4196537398c
Atila/Celular <atilalimade@gmail.com>
2025-07-11 16:32:17 -0300

    api: developing first Clients querys
    not tested, well implemented
    need to revise: db dates, requisits, how to implement it all

--- src/backend/services/purchases.py
+++ src/backend/services/purchases.py
@@ -2,6 +2,16 @@ from fastapi import Depends
 from sqlmodel import Session, select
 
 from backend.dependencies.connections import get_db_session_dependency
+from backend.models import Clients, Clients_Addresses
 
-def get_client_by_CPF(CPF: str, session: Session = Depends(get_db_session_dependency)):
-    ...
+def load_client_by_CNPJ(CNPJ: str, session: Session = Depends(get_db_session_dependency)):
+    stmt = select(Clients).where(Clients.CNPJ == CNPJ)
+    return session.exec(stmt).one_or_none()
+
+def load_client_by_CPF(CPF: str, session: Session = Depends(get_db_session_dependency)):
+    stmt = select(Clients).where(Clients.CPF == CPF)
+    return session.exec(stmt).one_or_none()
+
+def load_client_address(client: Client, session: Session = Depends(get_db_session_dependency)):
+    stmt = select(Clients.id, Clients_Addresses).join(Clients_Addresses).where(Clients.id == client.id).order_by(Clients.id)
+    return session.exec(stmt).first().one_or_none()
