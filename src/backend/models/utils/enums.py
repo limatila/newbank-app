7540d74d55d2633cfeddf3af868a6396247c03d0
Atila/Celular <atilalimade@gmail.com>
2025-07-14 17:29:42 -0300

    models: adding enum for useful payment types
    WARN: ERD MUST BE REVISED

--- src/backend/models/utils/enums.py
+++ src/backend/models/utils/enums.py
@@ -1,16 +1,38 @@
 from enum import Enum
 
-class ProfitIndex(str, Enum):
+class stringableEnum(str, Enum):
+    @classmethod
+    def from_str(cls, value: str):
+        return cls(value.lower())
+
+class ProfitIndex(stringableEnum):
     FIXED = "fixed"
     FLOATING = "floating"
     HYBRID = "hybrid"
 
-    @classmethod
-    def from_str(cls, value: str):
-        return cls(value.lower())
+class TypePayment(stringableEnum):
+    """
+    Types of payments: Invoices, PIX, TED, Withdraw, Credit Invoice
+    """
+    INVOICE = "invoice"
+    PIX = "pix"
+    WITHDRAW = "withdraw"
+    CREDIT_INVOICE = "credit"
+
+class TypeMethodPayment(stringableEnum):
+    """
+    Types of Payment Methods (which client can use)
+    """
+    PIX = "pix"
+    CARD = "card"
+    VIRTUAL_CARD = "virtual"
+    
 
 if __name__ == "__main__":
     i = ProfitIndex.from_str("floating")
     print(i)
     print( ProfitIndex.from_str('fIxED') )
     print(ProfitIndex.HYBRID.title())
+
+    j = TypePayment.from_str("pIx")
+    print(j.title())
