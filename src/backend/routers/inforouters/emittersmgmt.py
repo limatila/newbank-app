5e28665de4c4c0e3a4dbafb671c3c269a3976fc9
Átila Lima <atilalimade@gmail.com>
2025-07-22 16:45:33 -0300

    api: added feature to change emitters active status
    tested, working

--- src/backend/routers/inforouters/emittersmgmt.py
+++ src/backend/routers/inforouters/emittersmgmt.py
@@ -6,6 +6,7 @@ from backend.dependencies.connections import get_db_session
 from backend.services.infomgmt.emittersmgmt import (
     register_new_emitter,
     load_emitter_by_CNPJ,
+    change_emitter_active_status,
 )
 from backend.routers.utils.input_checkers import (
     transform_document_to_digits,
@@ -15,7 +16,7 @@ from backend.routers.utils.input_checkers import (
 
 emitters_router = APIRouter(prefix='/emitters', tags=['Emitter information'])
 
-#POST
+#* POST
 @emitters_router.post("/new")
 def new_client(CNPJ: str, name: str, active: bool = True, session: Session = Depends(get_db_session)):
     CNPJ = transform_document_to_digits(CNPJ, 'CNPJ')
@@ -29,16 +30,28 @@ def new_client(CNPJ: str, name: str, active: bool = True, session: Session = Dep
         return {"result": "sucess"}
     else: return {"result": "failure"}
 
-#GET
-@emitters_router.get("/load/CNPJ/{CNPJ_number}")
-def get_emitter_by_CNPJ(CNPJ_number: str, session: Session = Depends(get_db_session)):
-    CNPJ_number = transform_document_to_digits(CNPJ_number, 'CNPJ')
 
-    if not check_CNPJ_length(CNPJ_number):
+#* GET
+@emitters_router.get("/load/CNPJ")
+def get_emitter_by_CNPJ(CNPJ: str, session: Session = Depends(get_db_session)):
+    CNPJ = transform_document_to_digits(CNPJ, 'CNPJ')
+
+    if not check_CNPJ_length(CNPJ):
         raise HTTPException(detail=f"CNPJ is not valid, please assure it's {CNPJ_OFICIAL_LENGTH} characters long.", status_code=400)
 
-    result: bool = load_emitter_by_CNPJ(CNPJ_number, session)
+    result: bool = load_emitter_by_CNPJ(CNPJ, session)
 
     if result:
         return result
     else: raise HTTPException(detail="No emitter was found with this CNPJ.", status_code=404)
+
+
+#* PUT
+@emitters_router.put("/alter/status")
+def get_emitter_by_CNPJ(newStatus: bool, CNPJ: str, session: Session = Depends(get_db_session)):
+    CNPJ = transform_document_to_digits(CNPJ, 'CNPJ')
+
+    if not check_CNPJ_length(CNPJ):
+        raise HTTPException(detail=f"CNPJ is not valid, please assure it's {CNPJ_OFICIAL_LENGTH} characters long.", status_code=400)
+
+    return change_emitter_active_status(session, newStatus, CNPJ)
--- src/backend/services/infomgmt/emittersmgmt.py
+++ src/backend/services/infomgmt/emittersmgmt.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from fastapi import HTTPException
 from sqlmodel import Session, select
 from sqlalchemy.exc import IntegrityError
 
 from backend.models import Emitters
 
-# Create
+#* Create
 def register_new_emitter(CNPJ: str, name: str, session: Session, active: bool = True) -> bool: 
     try:
         newEmitter = Emitters(CNPJ=CNPJ, name=name, active=active)
@@ -17,11 +18,42 @@ def register_new_emitter(CNPJ: str, name: str, session: Session, active: bool = 
     return True if newEmitter else False
 
 
-# Getters
+#* Getters
 def load_emitter_by_CNPJ(CNPJ: str, session: Session):
     stmt = select(Emitters).where(Emitters.CNPJ == CNPJ)
-    return session.exec(stmt).one_or_none()
+    result = session.exec(stmt).one_or_none()
+    if not result: raise HTTPException(detail="No emitter was found for the sent document.", status_code=404)
+
+    return result
 
 
-#Alters #TODO
-def change_emitter_active_status(session: Session, newStatus: bool, CNPJ: str): ...
+#* Alters
+def change_emitter_active_status(session: Session, newStatus: bool, CNPJ: str):
+    emitter: Emitters = load_emitter_by_CNPJ(CNPJ, session)
+
+    wasDeactivated, wasEqualStatus = (False, False)
+
+    #modeling response
+    if newStatus == emitter.active:
+        wasEqualStatus = True
+
+    #alter it
+    if not wasEqualStatus:
+        emitter.active = newStatus
+    #adding deactivation date
+    if newStatus == False:
+        newDeactivationDate = datetime.now() 
+        emitter.date_deactivated = newDeactivationDate
+        wasDeactivated = True
+
+    if not wasEqualStatus:
+        session.add(emitter)
+        session.commit()
+
+    result = {
+        "result": "sucess",
+    }
+    if wasEqualStatus: result.update({"detail": f"Emitter was already at state {'active' if newStatus else 'deactivated'}"})
+    if wasDeactivated: result.update({"date_deactivated": emitter.date_deactivated})
+
+    return result
