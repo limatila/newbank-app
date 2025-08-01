273a5612afd20fb32bbeaf26ac6913534d9427bb
Átila Lima <atilalimade@gmail.com>
2025-08-01 20:18:32 -0300

    feat: added bulk loaders to load multiple data models
    tested, string filter not working

---
+++ src/backend/routers/bulks.py
@@ -0,0 +1,76 @@
+from typing import Any
+from datetime import datetime
+
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException
+from sqlmodel import Session
+
+from backend.dependencies.connections import get_db_session
+from backend.services.bulkLoaders import (
+    load_filtered_data,
+    load_ordered_data,
+    load_range_data
+)
+from backend.routers.utils.input_checkers import (
+    transform_document_to_digits,
+    CNPJ_OFICIAL_LENGTH,
+    CPF_OFICIAL_LENGTH,
+)
+
+bulk_router = APIRouter(prefix='/bulk', tags=['Bulk management'])
+
+@bulk_router.get("/load/{data_choice}")
+def load_multiple_data(data_choice: str, filter_option: str = None, filter_value: Any = None,
+                            order_option: str = None, order_orientation: str = "ASC",
+                            limit: int = 15, offset: int = 0, session: Session = Depends(get_db_session)):
+    #validating load_range entrys
+    if limit < 0: raise HTTPException("Limit cannot be 0 or negative.", status_code=403)
+    if offset < 0: raise HTTPException("Offset cannot be negative.", status_code=403)
+
+    result = load_range_data(session, data_choice, limit, offset, 
+                                _return_stmt=(True if filter_option else False))
+
+    #filtering
+    if filter_option and filter_value:
+        #validating filtering
+        filter_option = filter_option.strip().lower()
+        filter_value = filter_value.strip().lower()
+
+        if filter_option.upper() in ["CNPJ", "CPF"]:
+            filter_option = filter_option.upper()
+            filter_value = filter_value.upper()
+
+        try:
+            match(filter_option):
+                case "CNPJ":
+                    filter_value = transform_document_to_digits(filter_value, "CNPJ")
+                case "CPF":
+                    filter_value = transform_document_to_digits(filter_value, "CPF")
+                case _:
+                    try:
+                        filter_value = int(filter_value)
+                    except ValueError:
+                        ...
+        except AssertionError:
+            raise HTTPException(detail=f"Inserted {filter_option} is not valid. Please certify that the sent document has {CNPJ_OFICIAL_LENGTH if filter_option == "cnpj" else CPF_OFICIAL_LENGTH}", status_code=403) 
+
+        if 'date' in filter_option:
+            try:
+                filter_value = datetime.strptime(filter_value, "%d-%m-%Y").date()
+            except ValueError:
+                HTTPException(detail="Inserted date for filter_value is on wrong format. Please use day-month-year ISO format.", status_code=403)
+
+        result = load_filtered_data(session, result, filter_option, filter_value,
+                                        _return_stmt=(True if order_option else False))
+    elif not(filter_option and filter_value) and (filter_option or filter_value): 
+        raise HTTPException(detail="for filtering, filter_option and filter_value are needed.", status_code=403)
+
+    #ordering
+    if order_option:
+        #validating ordering
+        order_option = order_option.strip().lower()
+        order_orientation = order_orientation.strip().upper()
+        result = load_ordered_data(session, result, order_option, order_orientation)
+
+    #return all data
+    return result
---
+++ src/backend/services/bulkLoaders.py
@@ -0,0 +1,112 @@
+from typing import Any
+import inspect
+
+from fastapi import HTTPException
+from sqlalchemy import Select
+from sqlmodel import Sequence, Session, select, SQLModel
+
+from backend import models
+
+#load all models into a dict
+MODEL_CHOICES: dict[str, SQLModel] = { 
+    name.lower(): obj 
+    for name, obj in inspect.getmembers(models)
+    if issubclass(obj, SQLModel) and not (obj is SQLModel)
+}
+
+def load_range_data(session: Session, choice: str, limit: int = 15, offset: int = 0, _return_stmt = False) -> Sequence[SQLModel] | Select:
+    """loads a Sequence of any table at choice, or return a Select stmt to be processed in another function
+
+    Args:
+        model (SQLModel): the model to be loaded. Use one from backend.models to be searched here.
+        limit (int, optional): limit size of Sequence, cannot be less then 0. Defaults to 15. If 0, will ignore limit
+        offset (int, optional): positive offset of result, cannot be less then 0. Defaults to 0.
+
+    Returns:
+        Select | Sequence[Clients]: Returns the stmt or the loaded Clients
+    """
+
+    #get needed model
+    model: SQLModel = MODEL_CHOICES.get(str(choice).lower(), None)
+    if not model: raise HTTPException(detail="Your data_choice path is not referring to a existant model / table / data type.", status_code=403)
+
+    stmt = select(model).order_by(model.id)
+
+    if limit < 0: raise Exception("Limit cannot be 0 or negative.")
+    if limit != 0: stmt = stmt.limit(limit)
+
+    if offset:
+        if offset < 0: raise Exception("Offset cannot be negative.")
+        stmt = stmt.offset(offset)
+
+    if _return_stmt:
+        return stmt
+
+    result = session.exec(stmt).all()
+
+    if not result: raise HTTPException(detail="No Client was found for this criteria.", status_code=404)
+
+    return result
+
+def load_filtered_data(session: Session, stmt: Select, filterOption: str, filterValue: Any, _return_stmt = False) -> Sequence[SQLModel] | Select:
+    """Add filter to Select stmt
+
+    Returns:
+        Sequence[SQLModel] | Select: Result, or Stmt for selection enrichment
+    """
+
+    #Get the model class from the raw column
+    modelUsed = stmt.column_descriptions[0].get("entity", None)
+
+    if modelUsed is None:
+        raise ValueError("Could not determine model from the provided statement.")
+
+    attribute = getattr(modelUsed, filterOption, None)
+
+    if not attribute:
+        raise HTTPException(detail=f"The filter_option '{filterOption}' is not an attribute of model '{modelUsed.__name__}'", status_code=403)
+
+    stmt = stmt.where(attribute == filterValue)
+
+    if _return_stmt:
+        return stmt
+
+    result = session.exec(stmt).all()
+
+    if not result:
+        raise HTTPException(detail="No data was found for this criteria.", status_code=404)
+    
+    return result
+
+def load_ordered_data(session: Session, stmt: Select, orderOption: str, orderOrientation: str = "ASC", _return_stmt = False) -> Sequence[SQLModel] | Select:
+    """Add order to Select stmt
+
+    Returns:
+        Sequence[SQLModel] | Select: Result, or Stmt for selection enrichment
+    """
+
+    #Extract the first "raw column"
+    firstColumn = stmt._raw_columns[0]
+
+    #Get the model class from the raw column
+    modelUsed = getattr(firstColumn, "class_")
+    attribute = getattr(modelUsed, orderOption, None)
+
+    if not attribute:
+        raise HTTPException(f"The order_option used does not correspond to an attribute from {modelUsed.__name__}", status_code=403)
+
+    match(orderOrientation.upper()):
+        case "ASC":
+            stmt = stmt.order_by(attribute)
+        case "DESC":
+            stmt = stmt.order_by(attribute.desc())
+        case _: raise HTTPException("The order_orientation inserted is not valid. Please choose between 'ASC' and 'DESC' for orientation", status_code=403)
+
+    if _return_stmt:
+        return stmt
+
+    result = session.exec(stmt).all()
+
+    if not result: raise HTTPException(detail="No data was found for this criteria.", status_code=404)
+
+    return result
