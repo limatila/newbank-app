0c737bb1036a3e9115758fe759327f290ca8e1cc
Átila <atilalimade@gmail.com>
2026-03-31 21:53:42 -0300

    wip: copiando obtenção cliente por CPF

--- src/backend/services/scoremgmt.py
+++ src/backend/services/scoremgmt.py
@@ -1,7 +1,10 @@
 from fastapi import Depends
 from sqlmodel import Session, select
 
-from backend.dependencies.connections import get_db_session_dependency
+from backend.dependencies.connections import get_db_session
+from backend.models import Clients
 
-def get_client_by_CPF(CPF: str, session: Session = Depends(get_db_session_dependency)):
-    ...
+def load_client_by_CNPJ(CNPJ: str, session: Session = Depends(get_db_session)):
+    stmt = select(Clients).where(Clients.CNPJ == CNPJ)
+    return session.exec(stmt).one_or_none()
+
